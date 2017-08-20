7e4046e17b4ec8055b4f2ca66760a1013f634d5a
Catherine J <email": null>
2017-08-20 12:38:00 -0500

    No redirecting to welcome page for signin

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
 
   get 'home/index'
-
-  get 'welcome/home'
+  get 'home/home'
+  # get 'welcome/home'
 
   get "home", to: "home#index", as: "user_root"
   root 'home#index'
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  # resources :homes, only: [:index]
-  #   get '/homes/wiki' => "homes#wiki"
+  resources :homes, only: [:index]
+
 
   # root to: "homes#index"
 
