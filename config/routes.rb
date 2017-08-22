c8bfd439f69f9dc419f983002bcc3b269179a638
Brittany Restine <email": null>
2017-08-22 16:42:56 -0500

    Add redirect for after search when on homes/search already

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,10 @@
 Rails.application.routes.draw do
 
-  # get 'home/index'
-  # get 'home/home'
-  # # get 'welcome/home'
-
-  # get "home", to: "home#index", as: "user_root"
-  # # root 'home#index'
-
-  # devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  # resources :homes, only: [:index]
-
   get 'homes/index' => 'homes#index'
   post 'homes/search' => 'homes#search'
-
+  get 'homes/search', to: redirect('homes/index')
 
   root to: "homes#index"
-
-  # root 'songkicks#index'
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :users, only: [:show, :users, :new]
