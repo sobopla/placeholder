63a97ed93ae05c32cb5bd00c29ef90fa25618480
Brittany Restine <email": null>
2017-08-22 16:59:13 -0500

    Make search hide and show what is neccesary

--- app/assets/javascripts/search_bar.js
+++ app/assets/javascripts/search_bar.js
@@ -1,60 +1,34 @@
 
 $(document).ready(function(){
-  $(".loading").hide();
-  $(".load_more").hide();
+// HIDING LOGO AND SEARCH BAR ON SUBMIT TO SHOW THE BOPPIN BEFORE RESULTS LOAD
 
-  $("form.search-bar").on("submit", function(event){
-    event.preventDefault();
-    $(".loading").show();
-
-    var $form = $(this);
-    var page = $(".load_more").attr("data-page");
-    $form.hide();
-
-    var data = {
-      user_input: $("input.flexsearch--input").val(),
-      page: page
-    }
-
-    $.post("homes/search", data, function(response){
-      for (var i = 0; i < response.partials.length; i++) {
-      	$("div .results").append(response.partials[i])
-      }
-      $(".loading").hide();
-      $form.show();
-      $(".load_more").show();
-
-      var more = parseInt($("button.load_more").attr("data-page"));
-      $("button.load_more").attr("data-page", more + 1);
-    })
-
-    $(".load_more").on("click", function(event){
-      event.preventDefault();
-      console.log("you clicked me");
-      var button = $(this);
-      var page = button.data("page");
-
-      var data = {
-        user_input: $("input.flexsearch--input").val(),
-        page: page
-      }
-
-      $.post("homes/search", data, function(response){
-        console.log(response)
-        for (var i = 0; i < response.partials.length; i++) {
-          $("div .results").append(response.partials[i])
-        }
-        var more = parseInt($("button.load_more").attr("data-page"));
-        $("button.load_more").attr("data-page", more + 1);
-      })
-    })
+  $("form.search-bar").on("submit", function(){
+    $("div.logo-to-hide img").hide();
+    $("form").hide();
+    $("div.band-results").hide();
+    $("button#load-more-button").hide();
+    $(".loading").toggleClass("hidden")
 
   })
 
+// LOAD ONE MONTH MORE BUTTON FUNCTIONALITY
+  $("#load-more-button").on("click", function(event){
+    event.preventDefault();
+    var button = $(this);
+    var page = button.data("page");
+    var user_input = button.data("user-search");
 
-
-
-
-
+    var data = {
+      user_input: user_input,
+      page: page
+    }
+    $.post("/homes/search", data, function(response){
+      for (var i = 0; i < response.partials.length; i++) {
+        $("div .results").append(response.partials[i])
+      }
+      var more = parseInt($("button#load-more-button").attr("data-page"));
+      $("button#load-more-button").attr("data-page", more + 1);
+    })
+  })
 
 })
