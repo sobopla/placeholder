01d5b3b7da9151f5f8206d16b8fe96f53627e5fd
Brittany Restine <email": null>
2017-08-22 16:50:38 -0500

    ONly changed to make results display faster, but it stayed the same

--- app/helpers/page_helper.rb
+++ app/helpers/page_helper.rb
@@ -4,9 +4,9 @@ module PageHelper
     case page_counter
       when 0
         min_date = 1.hour.from_now.strftime("%Y-%m-%d")
-        max_date = 1.day.from_now.strftime("%Y-%m-%d")
+        max_date = 1.month.from_now.strftime("%Y-%m-%d")
       when 1
-        min_date = 1.month.from_now.strftimee("%Y-%m-%d")
+        min_date = 1.month.from_now.strftime("%Y-%m-%d")
         max_date = 2.months.from_now.strftime("%Y-%m-%d")
       when 2
         min_date = 2.months.from_now.strftime("%Y-%m-%d")
