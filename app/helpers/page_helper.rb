e4ec99b55289de693464abd799a9f2b70c14d7bb
Brittany Restine <email": null>
2017-08-23 17:34:35 -0500

    styling home and search page

--- app/helpers/page_helper.rb
+++ app/helpers/page_helper.rb
@@ -4,10 +4,10 @@ module PageHelper
     case page_counter
       when 0
         min_date = 1.hour.from_now.strftime("%Y-%m-%d")
-        max_date = 1.day.from_now.strftime("%Y-%m-%d")
+        max_date = 2.days.from_now.strftime("%Y-%m-%d")
       when 1
-        min_date = 2.days.from_now.strftime("%Y-%m-%d")
-        max_date = 3.days.from_now.strftime("%Y-%m-%d")
+        min_date = 3.days.from_now.strftime("%Y-%m-%d")
+        max_date = 4.days.from_now.strftime("%Y-%m-%d")
       when 2
         min_date = 2.months.from_now.strftime("%Y-%m-%d")
         max_date = 3.months.from_now.strftime("%Y-%m-%d")
