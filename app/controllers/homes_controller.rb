e5adfba2ac6b3ec3e6d8a2196efb234e91e47d7f
Brittany Restine & Joanna Jackson <pair+brittrestine.jo-hackson@devbootcamp.com>
2017-08-24 15:21:17 -0500

    Minor changes

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,7 +2,7 @@ class HomesController < ApplicationController
 
   def index
     @index_view = true
-    city = request.location.city
+    city = request.location.city # "Austin"
     if City.exists?(name: city)
       current_city = City.find_by(name: city)
       city_songkick_id = current_city.songkick
